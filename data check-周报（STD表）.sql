65cf41a7a0697c18cdad46bcd48e42248cc9baf5
bjzhangbiying <gwensharon@163.com>
2017-03-23 19:15:53 +0800

    20170322

--- data check-周报（STD表）.sql
+++ data check-周报（STD表）.sql
@@ -61,18 +61,7 @@ and ia.group_id in (2,3,4,5,6,9,10,11,12,105)
 
 
 
-select to_char(b1.firsttime,'yyyymmdd'), count(distinct b1.user_id) from
 
-(select a1.user_id,a1.firsttime from
-(select dis.user_id,min(dis.create_time) as firsttime from silver_consult.tb_crm_dispatch_his@consult_std dis where dis.ia_id is not null group by dis.user_id ) a1
-where to_char(a1.firsttime,'yyyymmdd') between 20170320 and 20170320) b1
-
-join silver_consult.tb_crm_dispatch_his@consult_std dis on b1.user_id= dis.user_id and b1.firsttime=dis.create_time
-join silver_consult.tb_crm_ia@consult_std ia on dis.ia_id=ia.id
-  join silver_consult.v_tb_crm_user@consult_std user1 on b1.user_id=user1.id and user1.id is not null
-
-where ia.group_id in (2,3,4,5,6,9,10,11,12,105) -- 新增资源数
-group by to_char(b1.firsttime,'yyyymmdd')
 
 
 
