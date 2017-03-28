53de485b44e159a40e62b9c8faa365020475c62c
bjzhangbiying <gwensharon@163.com>
2017-03-28 20:57:57 +0800

    20170328

--- data check-月报.sql
+++ data check-月报.sql
@@ -15,3 +15,70 @@ where to_char(submit_time,'yyyymmdd') <= 20170228) aa
   on aa.firm_id=bb.firmid
 where bb.fdate between 20170201 and 20170228
 
+
+select /*+driving_site(deal)*/ 4 as subid,
+   sum(case
+       when deal.wareid = 'GDAG' then deal.contnum*8
+       when deal.wareid = 'GDPD' then deal.contnum*1000*0.48
+       when deal.wareid = 'GDPT' then deal.contnum*1000*0.5 end) as money               -- 点差
+  from ods_history_deal@SILVER_STAT_URS_30_LINK deal
+  where deal.partner_id ='pmec'
+  and deal.operation_src = 'open'
+  and deal.fdate between 20170318 and 20170324
+  group by 4                           -- 点差
+
+select 5 as subid, sum(case when flow.changetype=8 then (-1)*flow.AMOUNT end) as money
+     from silver_njs.pmec_zj_flow flow
+     where to_char(flow.fdate,'yyyymmdd') between 20170318 and 20170324                   -- 滞纳金
+     group by 5
+
+select 6 as subid, sum(case when flow.changetype in (9,10) then (-1)*flow.amount end) as money -- 头寸+点差
+     from silver_njs.pmec_zj_flow flow
+      where to_char(flow.fdate,'yyyymmdd') between 20170318 and 20170324
+      group by 6
+
+select  /*+driving_site(deal)*/ 3 as subid,sum(deal.CONTQTY)*0.00065 as money                    -- 手续费
+   from ods_history_deal@silver_stat_urs_30_link deal
+   where deal.partner_id='pmec' and deal.fdate between 20170318 and 20170324
+   group by 3
+
+
+select /*driving_site(deal)*/
+  count(deal.firmid),
+  sum(deal.contqty)                                                                     --10万以上用户交易额
+from
+  ods_history_deal@silver_stat_urs_30_link deal
+JOIN
+  (
+    SELECT
+      firmid,
+      max(net_assets) AS assets
+    FROM silver_njs.tb_silver_data_center
+    WHERE hdate BETWEEN 20170201 AND 20170228
+    group by firmid
+    ) ass
+on deal.firmid=ass.firmid
+where ass.assets>=100000
+      and deal.partner_id='pmec'
+and deal.fdate between 20170201 and 20170228
+
+
+
+select sum(case when partnerid='pmec'  and inorout='A' then inoutmoney             ----广贵、南交净入金
+            when partnerid='pmec'  and inorout='B' then (-1)*inoutmoney end),
+  sum(case when partnerid='njs'  and inorout='A' then inoutmoney
+      when partnerid='njs'  and inorout='B' then (-1)*inoutmoney end)
+  from silver_njs.history_transfer
+where fdate between 20170201 and 20170228
+
+
+
+
+
+
+
+
+
+
+
+select * from ods_history_deal@silver_stat_urs_30_link
