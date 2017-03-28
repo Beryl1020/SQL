39c982d8e30cdc34e79a5d1b4ec3883fd7ab3c5b
bjzhangbiying <gwensharon@163.com>
2017-03-28 14:33:01 +0800

    20170328

--- data check-周报（STD表）.sql
+++ data check-周报（STD表）.sql
@@ -4,7 +4,7 @@ select '新增资源数', count(distinct b1.user_id) from
 
 (select a1.user_id,a1.firsttime from
 (select dis.user_id,min(dis.create_time) as firsttime from silver_consult.tb_crm_dispatch_his@consult_std dis where dis.ia_id is not null group by dis.user_id ) a1
-where to_char(a1.firsttime,'yyyymmdd') between 20170225 and 20170303) b1
+where to_char(a1.firsttime,'yyyymmdd') between 20170318 and 20170324) b1
 
 join silver_consult.tb_crm_dispatch_his@consult_std dis on b1.user_id= dis.user_id and b1.firsttime=dis.create_time
 join silver_consult.tb_crm_ia@consult_std ia on dis.ia_id=ia.id
@@ -18,7 +18,7 @@ select '新增A/B资源数', count(distinct b1.user_id) from
 
 (select a1.user_id,a1.firsttime from
 (select dis.user_id,min(dis.create_time) as firsttime from silver_consult.tb_crm_dispatch_his@consult_std dis where dis.ia_id is not null group by dis.user_id ) a1
-where to_char(a1.firsttime,'yyyymmdd') between 20170225 and 20170303) b1
+where to_char(a1.firsttime,'yyyymmdd') between 20170318 and 20170324) b1
 
 join silver_consult.tb_crm_dispatch_his@consult_std dis on b1.user_id= dis.user_id and b1.firsttime=dis.create_time
 join silver_consult.tb_crm_ia@consult_std ia on dis.ia_id=ia.id
