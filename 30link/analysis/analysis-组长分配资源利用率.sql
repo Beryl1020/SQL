fe22063aca60a277ad886db4fc077d68e170ddb1
bjzhangbiying <gwensharon@163.com>
2017-04-11 20:18:44 +0800

    20170411

--- 30link/analysis/analysis-组长分配资源利用率.sql
+++ 30link/analysis/analysis-组长分配资源利用率.sql
@@ -15,3 +15,11 @@ and to_char(a.create_time,'yyyymmdd')>=20170301
 group by to_char(a.create_time,'yyyymmdd')
 
 
+
+
+select * from info_silver.tb_crm_dispatch_his where ia_id in
+                                                    (647,151,657,131,678,530,202,732,735,259,287,792,261,610,361,374,621,421,927,596,767,167,731,899,608,884,885,663,656,883,733,153,529,160,528,924,219,791,154,640)
+and to_char(create_time,'yyyymmdd')=20170331
+select * from info_silver.tb_crm_ia where id in (151,131,530,732,259,261,361,621,927,596,167,899,884,663,883,153,529,160,528,924,640)
+)
+
